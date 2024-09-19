8b78a3d31527d72060419b3c7e7bf422bfe738cd
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-09-19 16:57:59 -0300

    1º projeto de react

---
+++ Aula-1-React/src/FuckSociety.jsx
@@ -0,0 +1,4 @@
+export default function FuckSociety()
+{
+    return "Fuck Society!"
+}
---
+++ Aula-1-React/src/main.jsx
@@ -0,0 +1,8 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import FuckSociety from './FuckSociety.jsx'
+
+
+createRoot(document.getElementById('root')).render(
+    <FuckSociety />
+)
